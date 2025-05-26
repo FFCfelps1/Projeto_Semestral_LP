d1b8fb69cb8ce17d365d6b063581e0f1283f40fa
Fernando Godoi Grinevicius <fernando.godoi.grinevicius@gmail.com>
2025-05-26 15:34:36 -0300

    Update User.java

--- Projeto_semestral(Quiz)/src/User.java
+++ Projeto_semestral(Quiz)/src/User.java
@@ -1,20 +1,26 @@
 public class User {
   private String name;
   private int score;
+  private String senha;
 
-  public User(String name) {
+  public User(String name, String senha) {
       this.name = name;
+      this.senha = senha;
       this.score = 0;
   }
 
   public String getName() {
       return name;
   }
-
+  public String getSenha() {
+        return senha;
+  }
+  public void setSenha(String senha) {
+      this.senha = senha;
+  }
   public int getScore() {
       return score;
   }
-
   public void addScore(int points) {
       this.score += points;
   }
